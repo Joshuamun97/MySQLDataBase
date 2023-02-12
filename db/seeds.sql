0efe6f327b4342f8ba85470c42f5dc707a2a378b
Joshua_Muniz <joshua.mun97@gmail.com>
2023-02-12 10:45:52 -0700

    updated database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,10 @@
-INSERT INTO department (name)
-VALUES  ("Electrical"),
-        ("Special Systems"),
-        ("ITS");
+INSERT INTO department (id, name)
+VALUES  (1, "Electrical"),
+        (2, "Special Systems"),
+        (3, "ITS");
+
+INSERT INTO roles (id, title, salary,)
+VALUES  (1, "Project Manager", 120,000),
+        (2, "General Foreman", 90,000),
+        (3, "Foreman", 75,000);
 
