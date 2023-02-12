d7025735ad1c293fa27c4be0bb36cc946bfb6251
Joshua_Muniz <joshua.mun97@gmail.com>
2023-02-12 16:19:34 -0700

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -14,7 +14,7 @@ id INT NOT NULL,
 title VARCHAR(30),
 salary DECIMAL(6, 0),
 department_id INT,
-PRIMARY KEY(id)
+PRIMARY KEY(id),
 FOREIGN KEY(department_id)
 REFERENCES department(id)
 );
@@ -25,5 +25,9 @@ first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INT,
 manager_id INT,
-PRIMARY KEY(id)
+PRIMARY KEY(id),
+FOREIGN KEY (role_id)
+REFERENCES roles(id),
+FOREIGN KEY (manager_id)
+REFERENCES roles(id)
 );
